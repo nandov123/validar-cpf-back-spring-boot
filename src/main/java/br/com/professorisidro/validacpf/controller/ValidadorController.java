fff9450119b5faa2f54deb5e0a0496170d5e2cdf
nandov123 <nando.valle@gmail.com>
2020-11-17 16:27:30 -0300

    home

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -19,4 +19,10 @@ public class ValidadorController {
 		 return "{\"status\": \"invalido\"}";
 	}
 
+	@GetMapping("/")
+	public String index() {
+		
+		 return "{\"mensagem\": \"Bem vindo à API do trainmento DevOps\"}";
+	}
+
 }
